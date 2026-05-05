a5838bbb4087a43e2c15db4bdfc477d7fd2cd903
johnsonfash <fashanutosin7@gmail.com>
2026-05-05 15:02:32 +0100

    first commit

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+
+export default async function handler(_: VercelRequest, res: VercelResponse) {
+  try {
+    // 1. Point to the index.html in your root directory
+    const filePath = path.join(process.cwd(), 'index.html');
+
+    // 2. Read the file
+    const html = readFileSync(filePath, 'utf8');
+
+    // 3. Send the response
+    res.setHeader('Content-Type', 'text/html');
+    return res.status(200).send(html);
+
+  } catch (error) {
+    return res.status(500).send("Error loading index.html. Make sure it is in the root folder.");
+  }
+}
